a56b184713854f89213b16494fbc34e1ac7777f0
Temuulen Ulziisaikhan <utemuka@gmail.com>
2022-01-19 02:30:55 -0800

    email thing

---
+++ javascript/actions.js
@@ -0,0 +1,6 @@
+function send_email(){
+    let first_name = document.querySelector('input#first_name').textContent;
+    let last_name = document.querySelector('input#last_name').textContent;
+
+    
+}
